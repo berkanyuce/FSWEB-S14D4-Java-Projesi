0d710938fd39d4915cc02129ae8ff51e6ba6bbb9
Berkan Yüce <berkanyuce33@gmail.com>
2023-11-28 21:40:05 +0300

    sorular çözüldü

--- src/com/s14Monster/Main.java
+++ src/com/s14Monster/Main.java
@@ -1,4 +1,14 @@
 package com.s14Monster;
 
 public class Main {
+    public static void main(String[] args) {
+        Troll troll = new Troll("Jane", 100,15.4);
+        Minion minion = new Minion("Red Minion", 25,5.5);
+        Champion pyke = new Champion("Pyke", 225,100.0);
+
+        System.out.println("Troll's attack damage: " + troll.attack());
+        System.out.println("Minion's attack damage: " + minion.attack());
+        System.out.println("Pyke's attack damage: " + pyke.attack());
+    }
+
 }
